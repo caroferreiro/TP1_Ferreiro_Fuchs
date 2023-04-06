41600128830ebb298d1091d6d0e912296a98d89e
Caro Ferreiro <carolina.ferreiro@mail.utdt.edu>
2023-04-06 17:54:16 -0300

    ll

---
+++ Parte2/ejercicio_1.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+#import csv
+
+# with open('ejercicio_1.csv', 'r') as datos:
+#     lector_csv = csv.reader(datos)
+    # Saltar la primera fila (encabezado)
+#    next(lector)
+    # x = []
+    # y = []
+    # for fila in lector:
+    #     x.append(float(fila[0]))
+    #     y.append(float(fila[1]))
+
+# plt.scatter(x, y)
+# plt.show()
+
+datos = pd.read_csv('archivo.csv', header=None)
+
+x = datos.iloc[:, 0].tolist()
+y = datos.iloc[:, 1].tolist()
+
+# Graficar puntos en el plano xy
+plt.scatter(x, y)
+plt.show()
+
