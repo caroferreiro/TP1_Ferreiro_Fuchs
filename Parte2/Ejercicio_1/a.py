0bc581622b976f6eee878fbfb2ffd1e0420a068f
Caro Ferreiro <carolina.ferreiro@mail.utdt.edu>
2023-04-11 08:39:09 -0300

    new

--- Parte2/Ejercicio_1/a.py
+++ Parte2/Ejercicio_1/a.py
@@ -1,19 +1,5 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-#import csv
-
-# with open('ejercicio_1.csv', 'r') as datos:
-#     lector_csv = csv.reader(datos)
-    # Saltar la primera fila (encabezado)
-#    next(lector)
-    # x = []
-    # y = []
-    # for fila in lector:
-    #     x.append(float(fila[0]))
-    #     y.append(float(fila[1]))
-
-# plt.scatter(x, y)
-# plt.show()
 
 datos = pd.read_csv('Parte2/Ejercicio_1/ejercicio_1.csv')
 
--- Parte2/Ejercicio_1/b.py
+++ Parte2/Ejercicio_1/b.py
@@ -7,23 +7,20 @@ datos = pd.read_csv('Parte2/Ejercicio_1/ejercicio_1.csv')
 x = datos.iloc[:, 0].tolist()
 y = datos.iloc[:, 1].tolist()
 
+X = np.array([[x[i]] for i in range(0,len(x))])
+Y = np.array([[y[i]] for i in range(0,len(y))])
+print(X,Y)
+
+beta = ((np.linalg.inv(np.transpose(X).dot(X))).dot(np.transpose(X))).dot(Y)
+print(beta)
+
+
 # Calcular la pendiente a y el intercepto b tal que y = ax + b
 pendiente, intercepto = np.polyfit(x, y, 1)
-
 print("Pendiente:", pendiente)
 print("Intercepto:", intercepto)
 
-# OTRA FORMA:
-x_mean = sum(x) / len(x)
-y_mean = sum(y) / len(y)
-
-a = sum((xi - x_mean) * (yi - y_mean) for xi, yi in zip(x, y)) / sum((xi - x_mean)**2 for xi in x)
-b = y_mean - a * x_mean
-
-print(f"La ecuación de la recta es: y = {a:.4f} + {b:.4f}x")
-
-beta = x
 # Gráficamente:
 plt.plot(x, y, 'o', color= '#000080')
-plt.plot(x, [a*xi+b for xi in x], color='OrangeRed')
+plt.plot(x, X.dot(beta), color='OrangeRed')
 plt.show()
