a1625bf299ed731786ee5f7f678d9077ce60cf60
Caro Ferreiro <carolina.ferreiro@mail.utdt.edu>
2023-04-11 08:59:34 -0300

    jj

--- Parte2/Ejercicio_1/a.py
+++ Parte2/Ejercicio_1/a.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-datos = pd.read_csv('ejercicio_1.csv')
+datos = pd.read_csv('Parte2/Ejercicio_1/ejercicio_1.csv')
 
 x = list(datos['X'].values)
 y = list(datos['Y'].values) 
